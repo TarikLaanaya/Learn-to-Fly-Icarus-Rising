17646da0916b2662d7561b8ba5b741777e0a04fe
Tarik <95280912+TarikLaanaya@users.noreply.github.com>
2025-12-08 08:00:29 +0000

    Started on main menu
    Just something quick that will allow me to clear the saved data for debugging the upgrades

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,9 +6,56 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public int towerHeight;
     [HideInInspector] public float playerStartHeight;
 
+    // --- Upgrades Data --- //
+    private const string wingUpgradeKey = "WingUpgrade";
+    private const string boostUpgradeKey = "BoostUpgrade";
+    private int currentWingUpgrade;
+    private int currentBoostUpgrade;
+
     void Awake()
     {
         towerHeight = towerStartHeight;
         playerStartHeight = towerHeight + 3.7f;
+
+        // If the key doesnt exist default to 0
+        currentWingUpgrade = PlayerPrefs.GetInt(wingUpgradeKey, 0);
+        currentBoostUpgrade = PlayerPrefs.GetInt(boostUpgradeKey, 0);
+    }
+
+    public void SetWingUpgrade(int upgradeAmount)
+    {
+        if (currentWingUpgrade > upgradeAmount) return;
+
+        PlayerPrefs.SetInt(wingUpgradeKey, upgradeAmount);
+        PlayerPrefs.Save();
+        currentWingUpgrade = upgradeAmount;
+    }
+
+    public void SetBoostUpgrade(int upgradeAmount)
+    {
+        if (currentBoostUpgrade > upgradeAmount) return;
+        
+        PlayerPrefs.SetInt(boostUpgradeKey, upgradeAmount);
+        PlayerPrefs.Save();
+        currentBoostUpgrade = upgradeAmount;
+    }
+
+    public int GetCurrentWingUpgrade()
+    {
+        return currentWingUpgrade;
+    }
+
+    public int GetCurrentBoostUpgrade()
+    {
+        return currentBoostUpgrade;
+    }
+
+
+    public void ClearData()
+    {
+        PlayerPrefs.DeleteKey(wingUpgradeKey);
+        PlayerPrefs.DeleteKey(boostUpgradeKey);
+
+        PlayerPrefs.Save();
     }
 }
